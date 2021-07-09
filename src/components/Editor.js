8437be8292662feedad63ec3a697668d07ff989d
Anushka <anushkabhayjoshi@gmail.com>
2021-07-10 00:07:48 +0530

    Scan and Run code

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,20 +6,11 @@ import 'ace-builds/src-noconflict/ext-language_tools';
 import 'ace-builds/src-noconflict/mode-java';
 import 'ace-builds/src-noconflict/mode-c_cpp';
 import 'ace-builds/src-noconflict/mode-python';
-import 'ace-builds/src-noconflict/mode-javascript';
-import 'ace-builds/src-noconflict/mode-rust';
-import 'ace-builds/src-noconflict/mode-kotlin';
 
 import 'ace-builds/src-noconflict/theme-github';
 import 'ace-builds/src-noconflict/theme-solarized_dark';
 import 'ace-builds/src-noconflict/theme-dracula';
 import 'ace-builds/src-noconflict/theme-monokai';
-import 'ace-builds/src-noconflict/theme-eclipse';
-import 'ace-builds/src-noconflict/theme-tomorrow_night';
-import 'ace-builds/src-noconflict/theme-tomorrow_night_blue';
-import 'ace-builds/src-noconflict/theme-xcode';
-import 'ace-builds/src-noconflict/theme-ambiance';
-import 'ace-builds/src-noconflict/theme-solarized_light';
 
 export default function Editor({ language,
     theme,
@@ -30,7 +21,25 @@ export default function Editor({ language,
     width,
     fontSize }) {
     return (
-          null
+        <AceEditor
+        mode={language}
+        theme={theme}
+        onChange={val => handleBodyChange(val)}
+        value={body}
+        width={width ? width : '100%'}
+        height={height ? height : '73vh'}
+        readOnly={readOnly ? readOnly : false}
+        fontSize={fontSize ? (isNaN(+fontSize) ? 12 : +fontSize) : 12}
+        name={language + theme}
+        showGutter={true}
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: true
+        }}
+    />
+
         
     )
 }
