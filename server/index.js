34c1f1549135635ea123a5f3667da1810f3d1c7b
vinayak-gaikwad <gaikwadvinayak291@gmail.com>
2021-07-10 19:07:12 +0530

    added login and register routes

--- server/index.js
+++ server/index.js
@@ -47,6 +47,73 @@ app.post('/upload', (req, res) => {
     })
 })
 
+//---------------------------------------Login / register------------------------------------//
+
+const getUsernames = async (user) => {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM users WHERE email = ?', [user.email], (error, elements) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(elements);
+        });
+    });
+}
+
+app.post('/register', async (req, res) => {
+    const { name, username, password } = req.body
+
+    console.log(name, username, password)
+
+    const hash = bcrypt.hashSync(password, 10); // saltRounds = 10
+    const user = { id: uuidv4(), name: name, email: username, password: hash, timestamp: new Date() }
+    const isValid = schema.validate(user)
+    
+    if(isValid.error) return res.send(isValid.error.details[0].message)
+
+    try {
+        const query = await getUsernames(user)
+
+        if (query.length === 0) {
+            const postQuery = await connection.query('INSERT INTO users SET ?', user)
+            return res.json({ registerStatus: true, id: user.id })
+        }
+        return res.send('user already exists')
+    }
+    catch (err) {
+        console.log(err)
+        return res.send('Error Occured')
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) return res.send('username and password required')
+
+    const user = { email: username, password: password }
+    try {
+
+        const query = await getUsernames(user)
+        //console.log(query.length)
+        if (query.length === 1) {
+            const isValid = bcrypt.compareSync(user.password, query[0].password);
+
+            if (isValid) {
+
+                return res.json({ auth: true, id: query[0].id })
+            }
+            return res.send('Wrong credintials')
+        }
+        return res.send('user does not exist, please register')
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+//---------------------------------------------------------------------------------------------------------------//
+
+
 //port
 const PORT = process.env.PORT || 5000
 // listening on port
