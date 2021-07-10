588beca887e69d42bd920927eb58567f45c2ecd7
Aditi Agwan <agwanaditi02@gmail.com>
2021-07-10 15:53:04 +0530

    Extra resources added

--- src/Resources/Resources.js
+++ src/Resources/Resources.js
@@ -182,6 +182,144 @@ const ResourcesList = [
 		name: "Python Learning Video",
 		topic: ['python'],
 		link: 'https://www.youtube.com/watch?v=WGJJIrtnfpk'
+	},
+
+	{
+		name: "Introduction to C",
+		topic: ['C'],
+		link: 'https://www.geeksforgeeks.org/c-language-set-1-introduction/'
+	},
+
+	{
+		name: "Installation in C",
+		topic: ['C'],
+		link: 'https://www.educba.com/install-c/'
+	},
+
+	{
+		name: "Keywords in C",
+		topic: ['C'],
+		link: 'https://www.w3schools.in/c-tutorial/keywords/'
+	},
+
+	{
+		name: "C Datatypes",
+		topic: ['C'],
+		link: 'https://www.tutorialspoint.com/cprogramming/c_data_types.htm'
+	},
+
+	{
+		name: "File Handling",
+		topic: ['C'],
+		link: 'https://www.javatpoint.com/file-handling-in-c'
+	},
+
+	{
+		name: "Files in C",
+		topic: ['C'],
+		link: 'https://www.youtube.com/watch?v=_KW_YBTXhN0'
+	},
+
+	{
+		name: "C Tutorial",
+		topic: ['C'],
+		link: 'https://www.youtube.com/watch?v=KJgsSFOSQv0'
+	},
+
+	{
+		name: "Arrays in C/C++",
+		topic: ['C', 'C++'],
+		link: "https://www.geeksforgeeks.org/arrays-in-c-cpp/"
+	},
+
+	{
+		name: "Arrays in C/C++",
+		topic: ['C', 'C++'],
+		link: "https://www.geeksforgeeks.org/pointers-c-examples/"
+	},
+
+	{
+		name: "C programming examples",
+		topic: ['C'],
+		link: 'https://www.programiz.Com/c-programming/examples'
+	},
+
+	{
+		name: "Quiz",
+		topic: ['C', 'C++'],
+		link: 'https://www.cprogramming.com/quiz/?inl=nv'
+	},
+
+	{
+		name: "Projects in C",
+		topic: ['C'],
+		link: 'https://learnprogramo.com/50-interesting-programming-c-projects-download-with-source-code/'
+	},
+
+	{
+		name: "OOPS Concepts",
+		topic: ['C++'],
+		link: "https://www.geeksforgeeks.org/object-oriented-programming-in-cpp/"
+	},
+
+	{
+		name: "Classes in C++",
+		topic: ['C++'],
+		link: "https://www.w3schools.com/cpp/cpp_classes.asp"
+	},
+
+	{
+		name: "C++ IDE installation",
+		topic: ['C++'],
+		link: "https://www.guru99.com/cpp-ide-download-install.html"
+	},
+
+	{
+		name: "C++ Tutorial",
+		topic: ['C++'],
+		link: "https://www.youtube.com/watch?v=BClS40yzssA"
+	},
+
+	{
+		name: "Inheritance in C++",
+		topic: ['C++'],
+		link: "https://www.javatpoint.com/cpp-inheritance"
+	},
+
+	{
+		name: "Polymorphism in C++",
+		topic: ['C++'],
+		link: "https://www.tutorialspoint.com/cplusplus/cpp_polymorphism.htm"
+	},
+
+	{
+		name: "Encapsulation in C++",
+		topic: ['C++'],
+		link: "https://beginnersbook.com/2017/09/cpp-encapsulation/"
+	},
+
+	{
+		name: "Access specifiers in C++",
+		topic: ['C++'],
+		link: "https://www.w3schools.com/cpp/cpp_access_specifiers.asp"
+	},
+
+	{
+		name: "File Handling in C++",
+		topic: ['C++'],
+		link: "https://www.tutorialspoint.com/cplusplus/cpp_files_streams.htm"
+	},
+
+	{
+		name: "Sample Programs",
+		topic: ['C++'],
+		link: "https://www.programiz.com/cpp-programming/examples"
+	},
+
+	{
+		name: "Difference between C and C++",
+		topic: ['C', 'C++'],
+		link: "https://www.geeksforgeeks.org/difference-between-c-and-c/"
 	}
 ]
 export default ResourcesList
