f599cc3fcd94746d1d6be2ce0de7a8c165621451
vinayak-gaikwad <gaikwadvinayak291@gmail.com>
2021-07-10 10:15:29 +0530

    setup google vision API

--- server/index.js
+++ server/index.js
@@ -1,10 +1,53 @@
 const express = require('express')
+const multer = require('multer')
+const cors = require('cors')
+const vision = require('@google-cloud/vision');
 
 const app = express();
 
+//middeleware
+app.use(cors())
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+var upload = multer({ storage: storage }).single('file')
+
+//getting code from image using google Vision
+app.post('/upload', (req, res) => {
+
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json(err)
+        }
+        else if (err) {
+            return res.status(500).json(err)
+        }
+
+        const client = new vision.ImageAnnotatorClient({
+            keyFilename: "./quickcode-318508-f48be9cc14dc.json",
+        })
+
+        client.documentTextDetection(req.file.path)
+            .then((results) => {
+                const text = results[0].fullTextAnnotation.text
+
+                return res.status(200).send({ file: req.file, text: text })
+
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
+})
+
 //port
 const PORT = process.env.PORT || 5000
-
 // listening on port
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import React, {useRef, useState} from 'react'
+import axios from 'axios'
+
 import './App.css';
 
 function App() {
+
+  const fileInput = useRef(null);
+
+  const visionAPI = () => {
+    const fileName = fileInput.current.files[0]
+    if (!fileName) alert("please select image")
+    const data = new FormData()
+    data.append('file', fileName)
+    axios.post('http://localhost:5000/upload', data, {})
+      .then(res => {
+        console.log(res.data.text)
+        // setBody(res.data.text)
+      })
+  }
+
+  const handleFileChange = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
-    <div className="App">
-      QuickCode
+    <div className="container-lg">
+      <div className="d-flex align-items-center justify-content-center mb-2" >
+        <label for="file-upload" class="custom-file-upload btn-outline-success">
+          <i class="fa fa-cloud-upload"></i> Upload Image
+        </label>
+
+        <input
+          id="file-upload"
+
+          type="file"
+          onChange={handleFileChange}
+          ref={fileInput}
+
+        />
+
+        <button class="btn btn-outline-success mx-3" onClick={visionAPI} >Get Code</button>
+
+      </div>
+
     </div>
   );
 }
