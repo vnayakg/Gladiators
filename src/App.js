17484afdd6504a033c497ee3b580b6721c5fe023
vinayak-gaikwad <gaikwadvinayak291@gmail.com>
2021-07-11 14:31:20 +0530

    added routing and navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react'
 import axios from 'axios'
-
+import{Route, Link} from 'react-router-dom'
 import Editor from './components/Editor';
 import SelectOption from './components/SelectOption';
 import NotesList from './components/NotesList';
@@ -199,6 +199,20 @@ function App() {
     setInput(value);
   };
 
+  const Navbar = () => {
+    return (
+      <div>
+
+        {isLoggedin
+          ?
+          <PersistentDrawerLeft loadNotes={loadNotes} navbarList={navbarList} currTitle={'QuickCode'} />
+          :
+          <PersistentDrawerLeft navbarList={signInList} currTitle={'QuickCode'} />
+        }
+      </div>
+    )
+  }
+
   const Notes = () => {
 
     return (
@@ -240,105 +254,181 @@ function App() {
 
   return (
     <div className="container-lg">
-      <div className="mb-2">
-        <SelectOption
-          label="Language"
-          defaultValue={language}
-          setValue={(val) => setLanguage(val)}
-          values={languages}
-        />
-      </div>
+      <Navbar />
+      <Switch>
+        {
+          isLoggedin ?
+            <>
 
-      <div className="mb-2">
-        <SelectOption
-          label="Theme"
-          defaultValue={theme}
-          setValue={setTheme}
-          values={themes}
-        />
+              <Route path="/login" render={(props) =>
+                <LoginPage {...props} setUserId={(id) => setUserId(id)} setIsLoggedIn={(val) => setIsLoggedIn(val)} />} />
+              <Route path="/register" component={Register} />
+              <Route path="/editor" >
+                <div>
+                  <div className="mb-2">
+                    <SelectOption
+                      label="Language"
+                      defaultValue={language}
+                      setValue={(val) => setLanguage(val)}
+                      values={languages}
+                    />
+                  </div>
 
-      </div>
-      <div className="mb-2">
-        <SelectOption
-          label="Font Size"
-          defaultValue={fontSize}
-          setValue={setFontSize}
-          values={fontSizes}
-        />
-      </div>
+                  <div className="mb-2">
+                    <SelectOption
+                      label="Theme"
+                      defaultValue={theme}
+                      setValue={setTheme}
+                      values={themes}
+                    />
 
-      <div className="d-flex align-items-center justify-content-center mb-2" >
-        <label for="file-upload" class="custom-file-upload btn-outline-success">
-          <i class="fa fa-cloud-upload"></i> Upload Image
-        </label>
+                  </div>
+                  <div className="mb-2">
+                    <SelectOption
+                      label="Font Size"
+                      defaultValue={fontSize}
+                      setValue={setFontSize}
+                      values={fontSizes}
+                    />
+                  </div>
 
-        <input
-          id="file-upload"
+                  <div className="col-lg-6 col-sm-12 mb-2 mt-3">
+                    <div className="d-flex align-items-center justify-content-between mb-2" >
+                      <label for="file-upload" class="custom-file-upload btn-outline-success">
+                        <i class="fa fa-cloud-upload"></i> Upload Image
+                      </label>
 
-          type="file"
-          onChange={handleFileChange}
-          ref={fileInput}
+                      <input
+                        id="file-upload"
 
-        />
+                        type="file"
+                        onChange={handleFileChange}
+                        ref={fileInput}
 
-        <button class="btn btn-outline-success mx-3" onClick={visionAPI} >Get Code</button>
-
-      </div>
-      <div>
-        <div className="row" >
-          <div className="col-lg-6 col-sm-12 mb-2 mx-0 pe-0" >
-            <p>EDITOR</p>
+                      />
 
 
-            <Editor
 
-              language={languageToEditorMode[language]}
-              theme={theme}
-              body={body}
-              handleBodyChange={(val) => handleUpdateBody(val)}
-              readOnly={false}
-              fontSize={fontSize}
-              width="100%"
-            />
-            <div className="d-flex justify-content-between my-2">
-              <button class="btn btn-outline-success" onClick={handleSubmit}  >Submit</button>
+                      <button class="btn btn-outline-success mx-3" onClick={visionAPI} >Get Code</button>
 
-              <button class="btn btn-outline-success" onClick={handleDownload}  >Download</button>
-            </div>
-          </div>
+                    </div>
 
-          <div className="col-lg-6 col-sm-12" >
-            <p>INPUT</p>
-            <Editor
-              language=''
-              theme={theme}
-              body={input}
-              handleBodyChange={handleUpdateInput}
-              readOnly={false}
-              fontSize={fontSize}
-              height='150px'
 
-            />
+                    {/* <div className="d-flex justify-content-between mb-2">
+                      <button class="btn btn-outline-success" onClick={handleSubmit} >Submit</button>
+                      <button class="btn btn-outline-success" onClick={handleDownload} >Download</button>
+                    </div> */}
 
-          </div>
-          <div className="col-sm-12" >
-            <p>OUTPUT</p>
-            <Editor
-              language=""
-              theme={theme}
-              body={output}
-              handleBodyChange={setOutput}
-              readOnly={true}
-              fontSize={fontSize}
-              height='250px'
+                    <div>
 
-            />
-          </div>
-        </div>
-      </div>
+                    </div>
 
+                  </div>
+                  <div className="row" >
+                    <div className="col-lg-6 col-sm-12 mb-2 mx-0 pe-0" >
+                      <p>EDITOR</p>
+
+
+                      <Editor
+
+                        language={languageToEditorMode[language]}
+                        theme={theme}
+                        body={body}
+                        handleBodyChange={(val) => handleUpdateBody(val)}
+                        readOnly={false}
+                        fontSize={fontSize}
+                        width="100%"
+                      />
+                      <div className="d-flex justify-content-between my-2">
+                        <button class="btn btn-outline-success" onClick={handleSubmit} >Submit</button>
+                        <button class="btn btn-outline-success" onClick={handleDownload} >Download</button>
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6 col-sm-12" >
+                      <p>INPUT</p>
+                      <Editor
+                        language=''
+                        theme={theme}
+                        body={input}
+                        handleBodyChange={handleUpdateInput}
+                        readOnly={false}
+                        fontSize={fontSize}
+                        height='150px'
+
+                      />
+
+                    </div>
+                    <div className="col-sm-12" >
+                      <p>OUTPUT</p>
+                      <Editor
+                        language=""
+                        theme={theme}
+                        body={output}
+                        handleBodyChange={setOutput}
+                        readOnly={true}
+                        fontSize={fontSize}
+                        height='250px'
+
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Route>
+              <Route path="/notes" >
+                <div className={`${darkMode && 'dark-mode'}`} >
+                  <Header handleToggleDarkMode={setDarkMode} />
+                  <Search handleSearchNote={setSearchText} />
+                  <NotesList
+                    notes={notes.filter((note) =>
+                      note.text.toLowerCase().includes(searchText)
+                    )}
+                    handleAddNote={addNote}
+                    handleDeleteNote={deleteNote}
+                  />
+                </div>
+              </Route>
+              <Route path="/resources" component={Resources} >
+                <div>
+                  <Search handleSearchNote={setResLang} />
+                  {
+
+                    ["Java", "Python", "C", "C++", "Javascript"].map((lang, ind) => {
+
+                      return (
+                        <button className="btn btn-outline-primary mx-3 my-3" value={lang} onClick={(lang) => setResLang(lang.target.innerText)} key={ind}><b>{lang}</b></button>
+                      )
+                    })
+
+                  }
+
+                  <CardList resources={ResourcesList.filter(itm => itm.topic[0].toLowerCase() === resLang.toLowerCase())} />
+                </div>
+              </Route>
+              <Route path="/logout" component={LogOut} />
+              <Route exact path='/' >
+                <Home isLoggedin={isLoggedin} />
+              </Route>
+            </>
+            :
+            <>
+              <Switch>
+                <Route path="/login" render={(props) =>
+                  <LoginPage {...props} setUserId={(id) => setUserId(id)} setIsLoggedIn={(val) => setIsLoggedIn(val)} />} />
+                <Route path="/register" component={Register} />
+                <Route exact path='/' >
+                  <Home isLoggedin={isLoggedin} />
+                </Route>
+                <Redirect from='*' to='/' />
+              </Switch>
+
+            </>
+        }
+
+      </Switch>
     </div>
-  );
+ 
+    );
 }
 
 export default App;
